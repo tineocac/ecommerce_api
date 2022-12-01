7ddcd5f3052b4708b52c65dca73155b4ccaf98bd
tineocac <carlostineocac@gmail.com>
2022-12-01 16:55:33 -0400

    feat: schemes and components of swagger on users was created

--- src/models/users.models.js
+++ src/models/users.models.js
@@ -2,6 +2,41 @@ const { DataTypes } = require("sequelize");
 const db = require("../utils/database");
 const bcrypt = require("bcrypt");
 
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     Users:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *         username:
+ *           type: string
+ *           example: Carlos Tineo
+ *         email:
+ *           type: string
+ *           example: ctineo@gmail.com
+ *     Register:
+ *       type: object
+ *       properties:
+ *         username:
+ *           type: string
+ *           example: Carlos Tineo
+ *         email:
+ *           type: string
+ *           example: tineoc@gmail.com
+ *         password:
+ *           type: string
+ *           example: 12345678
+ *   securitySchemes:
+ *      bearerAuth:
+ *        type: http
+ *        scheme: bearer
+ *        bearerFormat: JWT
+ */
+
 const Users = db.define(
   "users",
   {
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,6 +2,37 @@ const { Router } = require("express");
 const router = Router();
 const { createUser } = require("../controllers");
 
+/**
+ * @openapi
+ * /api/v1/users:
+ *   post:
+ *     summary: Create a new user into the app
+ *     tags: [Users]
+ *     requestBody:
+ *       description: To create a new user you need a username, email and password
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/Register"
+ *     responses:
+ *       201:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                      $ref: "#/components/schemas/Users"
+ *
+ */
+
 router.post("/users", createUser);
 
 module.exports = router;
